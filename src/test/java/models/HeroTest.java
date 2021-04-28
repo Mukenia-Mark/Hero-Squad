51456ef57b77c14296b6110fb336292533d1c807
Mukenia-Mark <Mark.mukenia@gmail.com>
2021-04-28 12:24:22 +0300

    add HeroTest class

---
+++ src/test/java/models/HeroTest.java
@@ -0,0 +1,44 @@
+package models;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HeroTest {
+  
+  @Test
+  public void NewHeroGetsCorrectlyCreated_true() throws Exception {
+    Hero hero = setupNewHero();
+    assertEquals(true, hero instanceof Hero);
+  }
+  
+  @Test
+  public void HeroInstantiatesWithName_true() throws Exception {
+    Hero hero = setupNewHero();
+    assertEquals("Rain-man", hero.getName());
+  }
+  
+  @Test
+  public void HeroInstantiatesWithAge_true() throws Exception {
+    Hero hero = setupNewHero();
+    assertEquals(27, hero.getAge());
+  }
+  
+  @Test
+  public void HeroInstantiatesWithPowers_true() throws Exception {
+    Hero hero = setupNewHero();
+    assertEquals("controls the weather", hero.getPowers());
+  }
+  
+  @Test
+  public void HeroInstantiatesWithWeakness_true() throws Exception {
+    Hero hero = setupNewHero();
+    assertEquals("being indoors", hero.getWeakness());
+  }
+  
+  
+  //helper methods
+  public Hero setupNewHero(){
+    return new Hero("Rain-man", 27, "controls the weather", "being indoors");
+  }
+  
+}
