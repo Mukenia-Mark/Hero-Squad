6bde4ff1e2bc1ac912ef4aebb885081dc8245041
Mukenia-Mark <Mark.mukenia@gmail.com>
2021-04-28 12:14:44 +0300

    add the create and drop sql files

---
+++ create.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE herosquad;
+\c herosquad;
+CREATE TABLE heroes (id SERIAL PRIMARY KEY, name VARCHAR, age INTEGER, powers TEXT,
+weakness TEXT);
+CREATE TABLE squad (id SERIAL PRIMARY KEY, name VARCHAR, cause TEXT, members VARCHAR)
+CREATE DATABASE herosquad_test WITH TEMPLATE herosquad;
