46c6d2aeb5e715532d7485703a726597002378b0
Mukenia-Mark <Mark.mukenia@gmail.com>
2021-04-28 12:18:47 +0300

    add the hero and squad model files

---
+++ src/main/java/models/Hero.java
@@ -0,0 +1,71 @@
+package models;
+
+import java.util.Objects;
+import java.util.ArrayList;
+
+public class Hero {
+  private String name;
+  private int age;
+  private String powers;
+  private String weakness;
+  private int id;
+  
+  public Hero(String name, int age, String powers, String weakness) {
+    this.name = name;
+    this.age = age;
+    this.powers = powers;
+    this.weakness = weakness;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public int getAge() {
+    return age;
+  }
+  
+  public void setAge(int age) {
+    this.age = age;
+  }
+  
+  public String getPowers() {
+    return powers;
+  }
+  
+  public void setPowers(String powers) {
+    this.powers = powers;
+  }
+  
+  public String getWeakness() {
+    return weakness;
+  }
+  
+  public void setWeakness(String weakness) {
+    this.weakness = weakness;
+  }
+  
+  @Override public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Hero hero = (Hero) o;
+    return age == hero.age && id == hero.id && Objects.equals(name, hero.name) &&
+      Objects.equals(powers, hero.powers) && Objects.equals(weakness, hero.weakness);
+  }
+  
+  @Override public int hashCode() {
+    return Objects.hash(name, age, powers, weakness, id);
+  }
+  
+  public int getId() {
+    return id;
+  }
+  
+  public void setId(int id) {
+    this.id = id;
+  }
+}
---
+++ src/main/java/models/Squad.java
@@ -0,0 +1,52 @@
+package models;
+
+import java.util.Objects;
+
+public class Squad {
+  private String name;
+  private String cause;
+  private String members;
+  
+  public Squad(String name) {
+    this.name = name;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getCause() {
+    return cause;
+  }
+  
+  public void setCause(String cause) {
+    this.cause = cause;
+  }
+  
+  public String getMembers() {
+    return members;
+  }
+  
+  public void setMembers(String members) {
+    this.members = members;
+  }
+  
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Squad squad = (Squad) o;
+    return Objects.equals(name, squad.name) &&
+           Objects.equals(cause, squad.cause) &&
+           Objects.equals(members, squad.members);
+  }
+  
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, cause, members);
+  }
+}
